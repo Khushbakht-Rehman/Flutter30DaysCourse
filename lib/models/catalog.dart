ee31a4eafcdfa33c06fa993e83d56502dc06acb7
Khushbakht Rehman <khushirehman85@gmail.com>
2021-07-29 17:11:51 +0500

    Day 10 Completed

---
+++ lib/models/catalog.dart
@@ -0,0 +1,27 @@
+class Item {
+  final String id;
+  final String name;
+  final String desc;
+  final num price;
+  final String color;
+  final String image;
+
+  Item(
+      {required this.id,
+      required this.name,
+      required this.desc,
+      required this.price,
+      required this.color,
+      required this.image});
+}
+
+final products = [
+  Item(
+      id: "1st",
+      name: "OPPO",
+      desc: "A1K",
+      price: 18000,
+      color: "#00ac51",
+      image:
+          "https://www.thesprucepets.com/thmb/UCGjJ5v6HgPJJ7OOtS5J3ijUBZ8=/1080x1350/filters:fill(auto,1)/35493166_2113126082300521_5592447779063463936_n-5b69b61946e0fb002562c234.jpg")
+];
