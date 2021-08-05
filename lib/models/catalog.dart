7510ff69eb330c3a0d99cd9907fdda25fa816c6c
Khushbakht Rehman <khushirehman85@gmail.com>
2021-08-05 11:28:28 +0500

    Day 12 Completed

--- lib/models/catalog.dart
+++ lib/models/catalog.dart
@@ -1,5 +1,18 @@
+class CatalogModel {
+  static final products = [
+    Item(
+        id: 1,
+        name: "OPPO",
+        desc: "A1K",
+        price: 18000,
+        color: "#00ac51",
+        image:
+            "https://www.thesprucepets.com/thmb/UCGjJ5v6HgPJJ7OOtS5J3ijUBZ8=/1080x1350/filters:fill(auto,1)/35493166_2113126082300521_5592447779063463936_n-5b69b61946e0fb002562c234.jpg")
+  ];
+}
+
 class Item {
-  final String id;
+  final num id;
   final String name;
   final String desc;
   final num price;
@@ -14,14 +27,3 @@ class Item {
       required this.color,
       required this.image});
 }
-
-final products = [
-  Item(
-      id: "1st",
-      name: "OPPO",
-      desc: "A1K",
-      price: 18000,
-      color: "#00ac51",
-      image:
-          "https://www.thesprucepets.com/thmb/UCGjJ5v6HgPJJ7OOtS5J3ijUBZ8=/1080x1350/filters:fill(auto,1)/35493166_2113126082300521_5592447779063463936_n-5b69b61946e0fb002562c234.jpg")
-];
--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,17 +1,23 @@
-import 'dart:html';
-
 import 'package:flutter/material.dart';
+import 'package:flutterapp/models/catalog.dart';
 import 'package:flutterapp/widgets/drawer.dart';
+import 'package:flutterapp/widgets/item_widget.dart';
 
 class HomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
+    final dummylist = List.generate(20, (index) => CatalogModel.products[0]);
     return Scaffold(
       appBar: AppBar(
         title: Text("Catalog App"),
       ),
-      body: Container(
-        child: Text("Hello world"),
+      body: Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: ListView.builder(
+            itemCount: dummylist.length,
+            itemBuilder: (context, index) {
+              return ItemWidget(item: dummylist[index]);
+            }),
       ),
       drawer: MyDrawer(),
     );
